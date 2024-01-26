9f4385d03c87bcb6cee199c8226d82012b5d5df5
Craig Dean <thargy@yahoo.com>
2024-01-26 12:29:44 +0000

    Added command line interpretation and keeps TCP connection alive.

--- README.md
+++ README.md
@@ -5,10 +5,13 @@ This is a prototype repository to attempt to create a fast scan of the modbus fo
 ## Usage
 
 ```bash
-python.exe scan.py <Modbus Server IP Address> [<port>]
+py scan.py <Modbus Server IP Address> [<port>]
 
-# e.g.
-python.exe scan.py 192.0.0.10
+# e.g. scans ids 1,5,6,7,8,9 using server at 192.0.0.10
+py scan.py 192.0.0.10 -d 1,5,6-9
+
+# for help
+py scan.py -h
 ```
 
 The port number defaults to `1502`.
--- scan.py
+++ scan.py
@@ -1,84 +1,242 @@
-import ipaddress
+from ipaddress import ip_address
 import select
+from time import sleep as sleep
 import socket
 import sys
+import argparse
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
+
+
+def port(value):
+    '''The function `port` takes a value as input and returns it as an integer if it is between 1024 and
+    49151, otherwise it raises an error.
+
+    Parameters
+    ----------
+    value
+        The `value` parameter is the input value that is being checked to see if it is a valid port number.
+
+    Returns
+    -------
+        the port number as an integer.
+
+    '''
+    p = int(value)
+    if p < 1024 or p > 49151:
+        raise argparse.ArgumentTypeError(
+            f"{value} must be between 1024 and 49151")
+    return p
+
+
+def retries(value):
+    '''The function `retries` takes a value as input and returns it as an integer if it is between 0 and
+    10, otherwise it raises an error.
+
+    Parameters
+    ----------
+    value
+        The `value` parameter is the input value that needs to be converted to an integer and checked if it
+    falls within the range of 0 to 10.
+
+    Returns
+    -------
+        the integer value of the input parameter after validating that it is between 0 and 10.
+
+    '''
+    r = int(value)
+    if r < 0 or r > 10:
+        raise argparse.ArgumentTypeError(f"{value} must be between 0 and 10")
+    return r
+
+
+def timeout(value):
+    '''The `timeout` function takes a value and returns it as a float if it is between 0.0 and 60.0
+    seconds, otherwise it raises an error.
+
+    Parameters
+    ----------
+    value
+        The value parameter represents the input value that is being checked for validity. In this case, it
+    is expected to be a string representing a floating-point number.
+
+    Returns
+    -------
+        a float representing the timeout value.
+
+    '''
+    t = float(value)
+    if t < 0.0 or t > 60.0:
+        raise argparse.ArgumentTypeError(
+            f"{value} must be between 0.0 and 60.0 seconds")
+    return t
+
+
+def device_id(value):
+    '''The `device_id` function takes a value and checks if it is a valid device ID between 1 and 247,
+    raising an error if it is not.
+
+    Parameters
+    ----------
+    value
+        The value parameter is the input value that is being checked for validity as a device ID.
+
+    Returns
+    -------
+        the device ID as an integer.
+
+    '''
+    id = int(value)
+    if (id < 1) or id > 247:
+        raise argparse.ArgumentTypeError(
+            f"'{value}' must be a device ID between 1 and 247")
+    return id
+
+
+def device_ids(value):
+    '''The function `device_ids` takes a string input and returns a list of device IDs, where the input can
+    be a single ID or a range of IDs separated by commas.
+
+    Parameters
+    ----------
+    value
+        The `value` parameter is a string that represents a list of device IDs. The device IDs can be
+    specified as individual IDs or as ranges separated by a hyphen. For example, the string "1,3-5,7"
+    represents the device IDs 1, 3, 4, 5 and 7
+
+    Returns
+    -------
+        The function `device_ids` returns a list of device IDs.
+
+    '''
+    parts = [p.strip() for p in value.split(',')]
+    ids = []
+    for p in parts:
+        r = [i.strip() for i in p.split('-')]
+        l = len(r)
+        if l < 2:
+            # We have a single id
+            ids.append(device_id(r[0]))
+
+        elif l > 2:
+            # Invalid range, multiple '-'s
+            raise argparse.ArgumentTypeError(
+                f"'{p}' in '{value}' looks like a range but has multiple '-'s.")
+
+        else:
+            # Looks like a range
+            start = device_id(r[0])
+            end = device_id(r[1])
+            if (end < start):
+                raise argparse.ArgumentTypeError(
+                    f"'{start}' must be less than or equal to {end}.")
+
+            ids.extend(range(start, end+1))
+
+    return sorted(set(ids))
+
+
+# Support console colors
+colorama_init()
+
+# Parse command line
+parser = argparse.ArgumentParser(
+    description='Performs a scan of TCP Modbus looking for device IDs.')
+
+parser.add_argument('ip-address', type=ip_address,
+                    help="The Modbus server to query.")
+parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+parser.add_argument('-d', '--device-ids', type=device_ids, metavar="N", required=False,
+                    help='The device ids to scan, can be comma-separated integers, or hypenated range, e.g. 1,2,4-7,10. Default is 1-247.', default="1-247")
+parser.add_argument('-p', '--port', type=port, required=False,
+                    metavar="P", help='The port number (1024-49151) of TCP Modbus. Default is 1502.', default=1502)
+parser.add_argument('-t', '--timeout', type=timeout, required=False,
+                    metavar="T", help='The timeout in seconds. Default is 3.0.', default=3.0)
+parser.add_argument('-r', '--retries', type=retries, metavar="N", required=False,
+                    help='The number of retries on a communication failure. Default is 3.', default=3)
+
+args = parser.parse_args()
+
+h = str(getattr(args, 'ip-address'))
+p = args.port
+t = args.timeout
+r = args.retries
+d = args.device_ids
+maxHex = 200
 
 #
 # Settings
 #
-timeout_seconds = 0.5 # Can increase this if not finding anything, larger is longer scan
 data = [0x0, 0x1, 0x0, 0x0, 0x0, 0x6, 0x0, 0x3, 0x9c, 0x40, 0x0, 0x45]
 device_id_index = 6
-max_device_id = 32 # 247 for full scan
-retries = 3
-
-#
-# Validate command line
-#
-arg_len = len(sys.argv)
-if arg_len < 2 or arg_len > 3 :
-    print("Usage: python scan.py <IP Address> [<port>]")
-    sys.exit(1)
-
-HOST = ''
-PORT = 1502
-
-# Parse IP Address
-try:
-    HOST = format(ipaddress.ip_address(sys.argv[1]))
-except ValueError as e:
-    print("Invalid IP Address {}".format(e))
-    sys.exit(1)
-    
-if (arg_len > 2):
-    # Parse port
-    try:
-        PORT = int(sys.argv[2])
-        if (PORT < 1024) or (PORT > 49151):
-            print("Port should be between 1024 and 49151")
-        sys.exit(1)
-    except ValueError as e:
-        print("Invalid Port {}".format(e))
-        sys.exit(1)
+connected = False
 
 #
 # Perform scan
 #
-device_id = 1
-while (device_id <= max_device_id):
+for device_id in d:
     # Update device_id
     data[device_id_index] = device_id
     attempt = 1
 
-    while (attempt <= retries ):
+    while not connected and attempt <= r:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            # Connect to
+            print(f"Connecting to {h}:{p} ... ", end= "")
+            sock.settimeout(t)
+            sock.connect((h, p))
+            connected = True  
+            print(f"{Fore.GREEN}SUCCEEDED{Fore.RESET}")
+        except socket.error as e:            
+            print(f"{Fore.RED}FAILED{Fore.RESET} {e}")
+            connected = False
+            attempt = attempt + 1
+            sleep(1.0)  
+
+    if (attempt > r):
+        print(f"{Fore.RED}Aborting due to {attempt-1} connection attempt failures! {Fore.RESET}")
+        sys.exit(1)
+    
+    while (attempt <= r):
         try:
             # Create a socket (SOCK_STREAM means a TCP socket)
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                # Connect to server and send data
-                sock.connect((HOST, PORT))
                 sock.setblocking(0)
                 sock.sendall(bytes(data))
                 print("Scanning ID: {} ... ". format(device_id), end='')
 
                 # Receive data from the server and shut down
-                ready = select.select([sock], [], [], timeout_seconds)
-                
+                ready = select.select([sock], [], [], t)
+
                 if ready[0]:
                     received = sock.recv(1024)
-                    
-                    print("Received: {}".format(received))
+
+                    print(f"{Fore.GREEN}Received{Fore.RESET} ({len(received)} bytes): { ' '.join(format(x, '02x') for x in received[:maxHex]) }{'...' if len(received) > maxHex else ''}")
                 else:
-                    print("Timedout")
+                    print(f"{Fore.RED}Timedout{Fore.RESET}")
                 break
-            
-        except ConnectionResetError as e:
+
+        except socket.error as e:
             print()
-            print("Connection reset {}".format(e))
+            print(f"{Fore.RED}FAILED{Fore.RESET}: {e}")
             attempt = attempt + 1
-    
-    if (attempt > retries):
-        print("Aborting due to multiple attempts")
+
+    if (attempt > r):
+        print(f"{Fore.RED}Aborting scanning device ID {device_id} after {attempt-1} attempts! {Fore.RESET}")
         break
     
-    device_id = device_id + 1
-            
+# Sanity check
+if (connected):
+    try:
+        # Connect to
+        print(f"Closing connection ... ", end= "")
+        sock.close()
+        print(f"{Fore.GREEN}SUCCEEDED{Fore.RESET}")
+    except socket.error as e:            
+        print(f"{Fore.RED}FAILED{Fore.RESET} {e}")
+    finally:
+        connected = False
+
+print()
+print("DONE!")
